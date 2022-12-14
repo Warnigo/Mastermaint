91104a88bb63e3f1adafe2da2762dee469d9cb19
warniGO <119101655+warniGO@users.noreply.github.com>
2022-12-14 18:28:12 +0500

    Create  mastermind.h

---
+++ mastermind.h
@@ -0,0 +1,14 @@
+#ifndef MY_MASTERMIND_H
+#define MY_MASTERMIND_H
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_piece {
+    int wellplcace;
+    int missplace;
+} piece;
+
+#endif
